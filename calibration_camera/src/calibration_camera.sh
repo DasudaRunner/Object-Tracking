141ed22d1f2382b6e7f9b28d10ad2cb7fb106cba
Dasuda <dasuda2015@163.com>
2018-08-21 08:55:22 +0800

    Update calibration_camera.sh

--- calibration_camera/src/calibration_camera.sh
+++ calibration_camera/src/calibration_camera.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+sudo chmod 777 demo
 cd ../imgs
+sudo chmod 777 imagelist_creator calibration
 ./imagelist_creator imagelist.yaml *.jpg
 ./calibration -w=7 -h=7 imagelist.yaml
 sudo mv out_camera_data.yml ../
